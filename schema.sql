31688a9899236ca3fd01a152d652b3a1dbbfe62d
JORGE CAMARGO <ji_camargo@yahoo.com>
2023-07-19 15:50:59 +0000

    update Schema.sql with new tables

--- schema.sql
+++ schema.sql
@@ -10,3 +10,16 @@ CREATE TABLE animals (
 /* project 2: add a new column: species */
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+/* project 3: Add a new tables  */
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
